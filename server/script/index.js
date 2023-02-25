5d5fbaf675672f07430e9853aed6d0edceb9573e
SunLxy <1011771396@qq.com>
2023-02-25 22:26:30 +0800

    fix:修改链接数据地址

--- server/script/index.js
+++ server/script/index.js
@@ -4,10 +4,15 @@ const fs = require('fs');
 const path = require('path');
 const CAREFREE_PASSWORD = process.env.CAREFREE_PASSWORD;
 const CAREFREE_USERNAME = process.env.CAREFREE_USERNAME;
+const CAREFREE_HOST = process.env.CAREFREE_HOST;
+const CAREFREE_PORT = process.env.CAREFREE_PORT;
+
 const temps = `
 export default {
   CAREFREE_PASSWORD:${JSON.stringify(CAREFREE_PASSWORD)},
   CAREFREE_USERNAME:${JSON.stringify(CAREFREE_USERNAME)},
+  CAREFREE_PORT:${JSON.stringify(CAREFREE_PORT)},
+  CAREFREE_HOST:${JSON.stringify(CAREFREE_HOST)},
 }
 `;
 
